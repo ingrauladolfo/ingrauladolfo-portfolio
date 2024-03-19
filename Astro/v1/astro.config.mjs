006f3d825dcbe4f239e534336511568245cb9795
Raúl Adolfo Torres Vargas <ingrauladolfo.torresvargas>
2024-03-18 21:46:12 -0600

    Finish the first two sections

--- Astro/v1/astro.config.mjs
+++ Astro/v1/astro.config.mjs
@@ -1,8 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: "https://ingrauladolfo-portfolio.vercel.app/",
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+    fallback: {
+      en: "en",
+    },
+  },
 });
