39c78de9c65b1f99e7385c5125fb2b4a443748bc
Raúl Adolfo Torres Vargas <ingrauladolfo.torresvargas@gmail.com>
2025-06-26 14:26:47 -0600

    reestructure React/v2

--- React/v2/src/assets/functions/getFormatLongDate.js
+++
@@ -1,9 +0,0 @@
-export const getFormatLongDate = (dateStr) => {
-    const [day, month, year] = dateStr.split('/').map(Number);
-    const date = new Date(year, month - 1, day);
-    return date.toLocaleDateString('es-ES', {
-      day: '2-digit',
-      month: 'long',
-      year: 'numeric'
-    });
-  };
